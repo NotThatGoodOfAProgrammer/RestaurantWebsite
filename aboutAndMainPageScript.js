bb8fac624ea47080208a4c124a30ec47f24a8cb1
NotThatGoodOfAProgrammer <109459018+NotThatGoodOfAProgrammer@users.noreply.github.com>
2023-03-01 16:54:20 +0100

    Add files via upload

---
+++ aboutAndMainPageScript.js
@@ -0,0 +1,36 @@
+addEventListener('DOMContentLoaded', (event) => {
+  const scroll = window.requestAnimationFrame ||
+              function(callback) {
+                window.setTimeout(callback, 1000/60)
+              };
+
+
+  function isElementInViewport(element) {
+    if (typeof jQuery === "function" && element instanceof jQuery) {
+      element = element[0];
+    }
+    let rect = element.getBoundingClientRect();
+    return (
+      (rect.top <= 0  &&  rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0  &&  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  };
+
+
+  const elementsToShow = document.querySelectorAll(".show-on-scroll");
+  function loop() {
+    elementsToShow.forEach(function (element)
+    {
+      if (isElementInViewport(element)) {
+        element.classList.add("is-visible");
+      };
+    });
+    scroll(loop);
+  };
+
+  loop();
+});
