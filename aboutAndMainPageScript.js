142959afdace83d7c47ea21383f8b72a0a748acc
Armatys Konrad <karmatys888@gmail.com>
2023-09-09 18:02:15 +0200

    fixing GitHub Pages

---
+++ aboutAndMainPageScript.js
@@ -0,0 +1,35 @@
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    (rect.top <= 0  &&  rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0  &&  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    &&
+    (rect.top != rect.bottom)
+  );
+};
+
+
+addEventListener('DOMContentLoaded', () => {
+  const scroll = window.requestAnimationFrame ||
+              function(callback) {
+                window.setTimeout(callback, 1000/60)
+              };
+
+
+  const elementsToShow = document.getElementsByClassName("show-on-scroll");
+  function loop() {
+    Array.from(elementsToShow).forEach(function (element)
+    {
+      if (isElementInViewport(element)) {
+        element.classList.add("visible");
+      };
+    });
+    scroll(loop);
+  };
+
+  loop();
+});
